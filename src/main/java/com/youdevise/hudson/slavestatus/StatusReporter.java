6f441e2a68b706ade135310024e3a81f28986bd4
jieryn <jieryn@dev.java.net>
2011-11-03 01:03:57 +0000

    [JENKINS-11590] migrate to github

--- src/main/java/com/youdevise/hudson/slavestatus/StatusReporter.java
+++
@@ -1,8 +0,0 @@
-package com.youdevise.hudson.slavestatus;
-
-import java.io.Serializable;
-
-public interface StatusReporter extends Serializable {
-    String getName();
-    String getContent();
-}
